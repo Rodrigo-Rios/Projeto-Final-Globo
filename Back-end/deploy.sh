ca876e025ee28ed6b6e7d92e91f4ff425dc28cfa
Francisco Carlos <carlos.junior@intmed.com.br>
2022-04-14 12:50:27 -0300

    adicionado sript de deploy no frontend

--- Back-end/deploy.sh
+++ Back-end/deploy.sh
@@ -1,3 +1,4 @@
+# git pull origin main # need to configure git ssh authentication
 docker build -t final-backend .
 docker rm -f final-backend
-docker run --name final-backend -d -p 80:5000 final-backend
+docker run --name final-backend -d -p 5000:5000 final-backend
